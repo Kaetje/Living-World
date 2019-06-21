5c2d490b6e47e8e4544ace09a5fbb835f5b06b80
Karin Westerbeek <kaetje@gmail.com>
2019-06-21 12:37:25 +0200

    database added

---
+++ dnd.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Jun 21, 2019 at 10:36 AM
+-- Server version: 5.7.26
+-- PHP Version: 7.3.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dnd`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `characters`
+--
+
+DROP TABLE IF EXISTS `characters`;
+CREATE TABLE IF NOT EXISTS `characters` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `CharacterName` varchar(255) NOT NULL,
+  `PlayerName` varchar(255) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `characters_xpevents`
+--
+
+DROP TABLE IF EXISTS `characters_xpevents`;
+CREATE TABLE IF NOT EXISTS `characters_xpevents` (
+  `character_id` int(11) NOT NULL,
+  `xpevent_id` int(11) NOT NULL,
+  UNIQUE KEY `character_id` (`character_id`,`xpevent_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `xpevents`
+--
+
+DROP TABLE IF EXISTS `xpevents`;
+CREATE TABLE IF NOT EXISTS `xpevents` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `sessionnumber` int(11) DEFAULT NULL,
+  `description` varchar(255) NOT NULL,
+  `xpamount` int(11) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
